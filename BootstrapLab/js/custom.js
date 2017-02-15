eeb82c41070883d3873b61f4199e3f30e0e7d0c8
Scott Yap <scottyzm@Scotts-MacBook-Pro.local>
2017-02-15 22:31:38 +0800

    Add form validation and jquery effects

--- BootstrapLab/js/custom.js
+++ BootstrapLab/js/custom.js
@@ -1,7 +1,19 @@
 $(document).ready(function() {
     $("#submit-button").click(function() {
-        if ($("#name").val().length === 0) {
-            alert("Please fill in your name");
-        }
+        $("input:blank").css("background-color", "#4286f4");
+        $("#contactForm").validate({
+            submitHandler: function(form) {
+                $.Zebra_Dialog('<p><strong>Thank you !</strong></p> ' +
+                    '<p>We will be in touch soon.</p>', {
+                        'type': 'confirmation'
+                    });
+            },
+            rules: {
+                cemail: {
+                    required: true,
+                    email: true
+                }
+            }
+        });
     });
 });
